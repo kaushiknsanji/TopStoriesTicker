1257e77a2cc24bbe5c8e6a136ef868eb03349a59
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2020-06-05 17:05:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/kaushiknsanji/topstoriesticker/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kaushiknsanji.topstoriesticker.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kaushiknsanji.topstoriesticker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kaushiknsanji/topstoriesticker/ui/custom/WindowBoxedImageView.kt
@@ -0,0 +1,123 @@
+package com.kaushiknsanji.topstoriesticker.ui.custom
+
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+
+/**
+ * Custom [AppCompatImageView] class that extends [AppCompatImageView.setFrame(int, int, int, int)]
+ * to resize/scale the image/drawable to 70 percent zoom in portrait mode
+ * and to 100 percent zoom in landscape mode. The images are cropped and centered in the View Frame.
+ * As with 70 percent zoom, images cropped will have black bars. This is known as Window Boxing,
+ * hence the name.
+ *
+ * @author Kaushik N Sanji
+ */
+class WindowBoxedImageView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    AppCompatImageView(context, attrs, defStyleAttr) {
+    //Constant for the Picture Zoom level in portrait mode
+    private val PORTRAIT_ZOOM_LEVEL = 0.70f
+
+    /**
+     * Method extended to resize/scale the image/drawable to 70 percent zoom in portrait mode
+     * and to 100 percent zoom in landscape mode (with occurrence of black bars in 70 percent zoom)
+     *
+     * @param frameLeft   is the Integer value of the position of the Left edge of the image frame
+     * @param frameTop    is the Integer value of the position of the Top edge of the image frame
+     * @param frameRight  is the Integer value of the position of the Right edge of the image frame
+     * @param frameBottom is the Integer value of the position of the Bottom edge of the image frame
+     * @return Boolean value. True if the new size and position are different than the previous ones
+     */
+    override fun setFrame(
+        frameLeft: Int,
+        frameTop: Int,
+        frameRight: Int,
+        frameBottom: Int
+    ): Boolean {
+        //Retrieving the current Drawable
+        val imageDrawable = drawable
+        if (imageDrawable != null) {
+            //If Drawable is present
+
+            //Retrieving the current screen orientation
+            val screenOrientation = resources.configuration.orientation
+            if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+                //When the device is in portrait mode
+
+                //Calculating the dimensions of the Frame
+                val frameWidth = frameRight - frameLeft.toFloat()
+                val frameHeight = frameBottom - frameTop.toFloat()
+
+                //Retrieving the dimensions of the Drawable
+                val imageDrawableWidth =
+                    imageDrawable.intrinsicWidth.toFloat()
+                val imageDrawableHeight =
+                    imageDrawable.intrinsicHeight.toFloat()
+
+                //Retrieving the current Image Matrix
+                val imageMatrix = imageMatrix
+
+                //Calculating the scale based on the image dimensions retaining its aspect ratio
+                val scale =
+                    if (imageDrawableWidth * frameHeight > frameWidth * imageDrawableHeight) {
+                        frameHeight / imageDrawableHeight * PORTRAIT_ZOOM_LEVEL
+                    } else {
+                        frameWidth / imageDrawableWidth * PORTRAIT_ZOOM_LEVEL
+                    }
+
+                //Centering the Image in the View Frame based on the scaled dimensions
+                val dx = (frameWidth - imageDrawableWidth * scale) * 0.5f
+                val dy = (frameHeight - imageDrawableHeight * scale) * 0.5f
+
+                //Applying a new Image Matrix with the scale calculated
+                imageMatrix.setScale(scale, scale)
+                //Translating the scaled image to the center of the frame
+                imageMatrix.postTranslate(
+                    kotlin.math.round(dx),
+                    kotlin.math.round(dy)
+                )
+
+                //Set scale type to Matrix
+                scaleType = ScaleType.MATRIX
+
+                //Pass the modified Matrix as the Image Matrix to be used
+                setImageMatrix(imageMatrix)
+            } else {
+
+                //Set scale type to Center Crop
+                scaleType = ScaleType.CENTER_CROP
+            }
+        }
+
+        //Calling to super and returning its value
+        return super.setFrame(frameLeft, frameTop, frameRight, frameBottom)
+    }
+
+    /**
+     * Sets a Bitmap as the content of this ImageView.
+     *
+     * @param bm The bitmap to set
+     */
+    override fun setImageBitmap(bm: Bitmap?) {
+        //Applying a black background for the black bar appearance
+        setBackgroundColor(ContextCompat.getColor(context, android.R.color.black))
+        //Propagating the call to super class
+        super.setImageBitmap(bm)
+    }
+
+    /**
+     * Sets a drawable as the content of this ImageView.
+     *
+     * @param resId the resource identifier of the drawable
+     */
+    override fun setImageResource(resId: Int) {
+        //Applying a black background for the black bar appearance
+        setBackgroundColor(ContextCompat.getColor(context, android.R.color.black))
+        //Propagating the call to super class
+        super.setImageResource(resId)
+    }
+
+}
